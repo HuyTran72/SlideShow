59f6171aa1e535c9fe4f7d730b82c35d0207f99b
Huy Tran <baohuy82.th@gmail.com>
2024-11-16 01:05:28 -0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 const listImage = document.querySelectorAll('.list-images');
 const imgs = document.getElementsByTagName('img');
 
-setInterval(() => {
+function next() {
     let width = imgs[0].offsetWidth
-    listImage.style.transform = `translateX(${width * -1}px)`
-}, 4000);
+    listImage.style.transform = `translateX(-${width}px)`;
+}
+
+setInterval(next, 4000);
+
+// setInterval(() => {
+//     let width = imgs[0].offsetWidth
+//     listImage.style.transform = `translateX(-${width}px)`;
+// }, 4000);
