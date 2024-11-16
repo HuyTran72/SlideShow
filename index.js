3660744a824a2efd137b3e6f2a7b8ac6c2983eff
Huy Tran <baohuy82.th@gmail.com>
2024-11-16 01:20:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
-const listImage = document.querySelector('.list-images')
-const imgs = document.getElementsByTagName('img')
+const listImage = document.querySelector('.list-images');
+const imgs = document.getElementsByTagName('img');
+let current = 0;
 
 setInterval(() => {
+    current ++;
     let width = imgs[0].offsetWidth
-    listImage.style.transform = `translateX(-${width}px)`;
+    listImage.style.transform = `translateX(-${width*current}px)`;
 }, 3000);
