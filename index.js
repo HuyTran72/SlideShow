d3b3223e63d4b5eb01b53eff94c4af2a1c4f0fff
Huy Tran <baohuy82.th@gmail.com>
2024-11-22 23:51:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const btnRight = document.querySelector('.btn-right');
 const length = imgs.length;
 let current = 0;
 
+const handleChangeSlide = () => {
+    if(current == length - 1) {
+        current = 0;
+        listImage.style.transform = `translateX(0px)`;
+    } else {
+        current ++;
+        let width = imgs[0].offsetWidth;
+        listImage.style.transform = `translateX(-${width*current}px)`;
+    }
+}
 setInterval(() => {
     if(current == length - 1) {
         current = 0;
